ab9ea755ac815581aabe9a6652fe2b66a5bd266e
Lucas Porto Ribeiro <lucaasportoasis@gmail.com>
2025-03-03 13:32:28 -0300

    Time: 0 ms (100%), Space: 8.3 MB (89.28%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -2,26 +2,10 @@ class Solution {
 public:
     string reversePrefix(string word, char ch) {
 
-        int ch_index = -1;
-
-        for(int i=0; i<word.length(); ++i)
-            if(word[i] == ch)
-            {
-                ch_index = i;
-                break;
-            }
-                
+        int ch_index = word.find(ch);
 
         if(ch_index != -1)
-        {
-            int next_change = ch_index;
-
-            for(int i=0; i<=ch_index/2; ++i)
-            {
-                swap(word[i], word[next_change]);
-                --next_change;
-            }
-        }
+            reverse(word.begin(), word.begin() + ch_index + 1);
 
         return word;
     }
